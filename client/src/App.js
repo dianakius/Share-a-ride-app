bd76921aa3d5840712e5fb34e5ca133a69e56483
dianakius <eingidiana@gmail.com>
2024-04-29 13:42:46 +0300

    Login and Register Form

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
-import Login from "./components/Login";
-import Register from "./components/Register";
+import React, { useState} from "react";
+import { Login } from "./components/Login";
+import { Register} from "./components/Register";
 import Home  from "./components/Home";
 import Ride from "./components/Ride"
 import { BrowserRouter, Routes, Route } from "react-router-dom";
@@ -9,10 +10,18 @@ import "./App.css";
 
 
 function App() {
+  const [currentForm, setCurrentForm] = useState('login');
+const toggleForm = (formName) => {
+  setCurrentForm(formName);
+}
+
   return (
     <BrowserRouter>
     <Navbar />
-    <div className="parallax"> 
+    <div className="App"> 
+    {
+      currentForm === "login" ? <Login onFormSwitch={toggleForm} /> : <Register  onFormSwitch={toggleForm}/>
+    }
     <Routes>
       <Route path="/login" element={<Login />} />
       <Route path="/signup" element={<Register/>}/> 
--- client/src/components/Register.js
+++ client/src/components/Register.js
@@ -3,40 +3,70 @@ import "./form.css";
 import axios from "axios";
 import "./navbar.css";
 
-function Register() {
-    const [username, setUsername] = useState("");
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
-    const [name, setName] = useState("");
-    const [surname, setSurname] = useState("");
-    const [dob, setDob] = useState("");
+export const Register = (props) => {
+    const [email, setEmail] = useState('');
+    const [pass, setPass] = useState('');
+    const [name, setName] = useState('');
 
-    const register = async (event) => {
-        event.preventDefault();
-        try {
-            let user = { username, email, password, name, surname, dob };
-            let res = await axios.post("http://localhost:3000/user/register", user);
-            alert(res.data.msg);
-        } catch (error) {
-         
-        }
-    };
+    const handleSubmit = (e) => {
+        email.preventDefault();
+        console.log(email);
 
+    }
     return (
-        <div className="form-container">
-            <div className="parallax"></div>
-            <h1>Register</h1>
-            <form className="registration-form" onSubmit={register}>
-                <input type="text" placeholder="Username" onChange={(e) => setUsername(e.target.value)} />
-                <input type="email" placeholder="Email" onChange={(e) => setEmail(e.target.value)} />
-                <input type="password" placeholder="Password" onChange={(e) => setPassword(e.target.value)} />
-                <input type="text" placeholder="Name" onChange={(e) => setName(e.target.value)} />
-                <input type="text" placeholder="Surname" onChange={(e) => setSurname(e.target.value)} />
-                <input type="date" placeholder="Date of Birth" onChange={(e) => setDob(e.target.value)} />
-                <input type="submit" value="Signup" />
-            </form>
+        <div className="auth-form-container">
+            <h2>Register</h2>
+        <form className="register-form"onSubmit={handleSubmit}>
+            <label htmlFor="name">Full Name</label>
+            <input value={name} name="name" id="name" placeholder="full Name"></input>
+            <label htmlFor="email">email</label>
+            <input value={email} onChange={(e) => setEmail(e.target.value)}type="email" placeholder="youremail@gmail.com" id="email" name="email" />
+            <label for="password">password</label>
+            <input value={email}type="password" placeholder="********" id="password" name="password" />
+            <button type="submit">Log In</button>
+        </form>
+        <button className="link-btn" onClick={() =>props.onFormSwitch('login')}>Already have an account? Login here.</button>
         </div>
-    );
+    )
 }
 
-export default Register;
+
+
+
+// function Register() {
+//     const [username, setUsername] = useState("");
+//     const [email, setEmail] = useState("");
+//     const [password, setPassword] = useState("");
+//     const [name, setName] = useState("");
+//     const [surname, setSurname] = useState("");
+//     const [dob, setDob] = useState("");
+
+//     const register = async (event) => {
+//         event.preventDefault();
+//         try {
+//             let user = { username, email, password, name, surname, dob };
+//             let res = await axios.post("http://localhost:3000/user/register", user);
+//             alert(res.data.msg);
+//         } catch (error) {
+         
+//         }
+//     };
+
+//     return (
+//         <div className="form-container">
+//             <div className="parallax"></div>
+//             <h1>Register</h1>
+//             <form className="registration-form" onSubmit={register}>
+//                 <input type="text" placeholder="Username" onChange={(e) => setUsername(e.target.value)} />
+//                 <input type="email" placeholder="Email" onChange={(e) => setEmail(e.target.value)} />
+//                 <input type="password" placeholder="Password" onChange={(e) => setPassword(e.target.value)} />
+//                 <input type="text" placeholder="Name" onChange={(e) => setName(e.target.value)} />
+//                 <input type="text" placeholder="Surname" onChange={(e) => setSurname(e.target.value)} />
+//                 <input type="date" placeholder="Date of Birth" onChange={(e) => setDob(e.target.value)} />
+//                 <input type="submit" value="Signup" />
+//             </form>
+//         </div>
+//     );
+// }
+
+// export default Register;
