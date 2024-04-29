673e7e7ea6d01bc63c8f9f0e28a4ee3ccdc3b3ca
dianakius <eingidiana@gmail.com>
2024-04-29 17:58:28 +0300

    Navbar updated

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,64 +1,128 @@
-import React from "react";
+import React, {useState} from "react";
 import { Link } from "react-router-dom";
 import "./navbar.css";
 import {jwtDecode } from "jwt-decode";
+import { Button } from "./Button";
 
-function Navbar() {
-    let token;
-    let decoded;
-    /*if (localStorage.getItem("token")){
-        token = localStorage.getItem("token");
-        decoded =jwtDecode(token);
-        console.log(decoded);
+
+function  Navbar () {
+    const [click, setClick] =useState(false);
+    const [button, setButton] = useState(true);
+
+    const handleClick = () => setClick(!click);
+    const closeMobileMenu = () => setClick(false);
+
+const showButton = () => {
+    if(window.innerWidth <= 960) {
+        setButton(false);
+    }else  {
+        setButton(true);
+   }
+};
+
+window.addEventListener('resize', showButton);
+
+     return (
+        <>
+<nav className="navbar">
+<div className="navbar-container">
+    <Link to="/" className="navbar-logo">
+        Share Ride
+    </Link>
+<div className="menu-icon" onClick={handleClick}>
+    <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
+    </div>
+    <ul className={click ? 'nav-menu active' : 'nav-menu'}>
+        <li className="nav-item">
+            <Link to='/' className="nav-links" onClick={closeMobileMenu}>
+                Home
+            </Link>
+        </li>
+        <li className="nav-item">
+            <Link to='/Login' className="nav-links" onClick={closeMobileMenu}>
+                Login
+            </Link>
+        </li>
+        <li className="nav-item">
+            <Link to='/Register' className="nav-links" onClick={closeMobileMenu}>
+                Register
+            </Link>
+        </li>
+        <li className="nav-item">
+            <Link to='/contact us' className="nav-links" onClick={closeMobileMenu}>
+                Contact Us
+            </Link>
+        </li>
+    </ul>
+    {button && <Button buttonStyle="btn--outline">SIGN UP</Button>}
+</div>
+</nav>
+
+     </>
+    )
+}
+
+export default Navbar
+
+
+
+
+// function Navbar() {
+//     let token;
+//     let decoded;
+//     /*if (localStorage.getItem("token")){
+//         token = localStorage.getItem("token");
+//         decoded =jwtDecode(token);
+//         console.log(decoded);
     
-    }*/
-    token = localStorage.getItem("token");
-    console.log(token)
-    if (token) {
-        decoded =jwtDecode(token);
-        console.log(decoded);
-    }
-function handleLogout() {
-    if (localStorage.getItem("token")) {
-        if(window.confirm("Are you sure you want to leave us?")) {
-        localStorage.removeItem("token");
-        alert("Logout");
-    }
-} else {
-    return;
-}
-}
+//     }*/
+//     token = localStorage.getItem("token");
+//     console.log(token)
+//     if (token) {
+//         decoded =jwtDecode(token);
+//         console.log(decoded);
+//     }
+// function handleLogout() {
+//     if (localStorage.getItem("token")) {
+//         if(window.confirm("Are you sure you want to leave us?")) {
+//         localStorage.removeItem("token");
+//         alert("Logout");
+//     }
+// } else {
+//     return;
+// }
+// }
 
-    return (
-        <div className="nav-container"> 
-        <div className="logo">
-        <a href="/">
-            <img src="LogoNew.png" alt="Logo" style={{ width: '300px', height: 'auto' }} />
-        </a>
-        </div>          
-      {token ? (
-        <div>
+//     return (
+//         <div className="nav-container"> 
+//         <div className="logo">
+//         <a href="/">
+//             <img src="LogoNew.png" alt="Logo" style={{ width: '300px', height: 'auto' }} />
+//         </a>
+//         </div>          
+//       {token ? (
+//         <div>
            
-    <div className="links"> 
-    <Link to="/profile">{decoded.email}</Link>
-    <Link to="/about-us">About Us</Link>
-    <Link to="/new" >Announce your Ride</Link>    
-    <Link onClick={handleLogout} to="/">Logout</Link>
-        </div>
-        </div>
-    ) : (
-            <div>
+//     <div className="links"> 
+//     <Link to="/profile">{decoded.email}</Link>
+//     <Link to="/about-us">About Us</Link>
+//     <Link to="/new" >Announce your Ride</Link>    
+//     <Link onClick={handleLogout} to="/">Logout</Link>
+//         </div>
+//         </div>
+//     ) : (
+//             <div>
             
-    <div className="links">  
-        <Link to="/ride">Announce</Link>
-        <Link to ="/login">Login</Link>
-        <Link to="/signup">Signup</Link>
-        </div>
-        </div>
-        )}
-     </div>
+//     <div className="links">  
+//         <Link to="/ride">Announce</Link>
+//         <Link to ="/login">Login</Link>
+//         <Link to="/signup">Signup</Link>
+//         </div>
+//         </div>
+//         )}
+//      </div>
   
-    );
-}
+//     );
+// }
 
-export default Navbar;
+// export default Navbar;
