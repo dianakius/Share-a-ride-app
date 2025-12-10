c4c3fd4c9d554def5f841a9552a61955f083888d
dianakius <eingidiana@gmail.com>
2025-12-10 18:46:17 +0200

    fix: change import to default imports

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { AuthProvider } from "./components/ContextAuth";
-import {Navbar} from "./components/Navbar";
-import {Login} from "./components/Login";
-import {Signup} from "./components/Signup";
-import {Home} from "./components/Home";
-import {Ride} from "./components/Ride";
-import {Footer} from "./components/Footer";
+import Navbar from "./components/Navbar";
+import Login from "./components/Login";
+import Signup from "./components/Signup";
+import Home from "./components/Home";
+import Ride from "./components/Ride";
+import Footer from "./components/Footer";
 
 function App() {
   return (
@@ -14,7 +14,7 @@ function App() {
       <BrowserRouter>
         <div className="min-h-screen flex flex-col">
           <Navbar />
-          <main className="flex-grow">
+          <main className="grow">
             <Routes>
               <Route path="/login" element={<Login />} />
               <Route path="/signup" element={<Signup />} />
