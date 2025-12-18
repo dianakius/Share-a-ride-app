f4ab4b7fce3a0dbcf5518abd8eb58398ccd21654
dianakius <eingidiana@gmail.com>
2025-12-18 11:28:41 +0200

    refactor: create date range for the entire day

--- backend/controllers/rideController.js
+++ backend/controllers/rideController.js
@@ -2,111 +2,71 @@ const { response } = require("express");
 const Ride = require("../models/rideModel");
 
 const searchRides = async (req, res) => {
-    try {
-        const { startLocation, destination, dateTime } = req.query;
-        
-        // Perform the search based on the query parameters
-        const rides = await Ride.find({
-            startingPoint: { $regex: new RegExp(startLocation, "i") }, // Case-insensitive search
-            endingPoint: { $regex: new RegExp(destination, "i") },
-            date: new Date(dateTime)
-        });
+  try {
+    const { startLocation, destination, dateTime } = req.query;
 
-        res.send(rides);
-    } catch (error) {
-        console.error("Error searching for rides:", error);
-        res.status(500).send({ msg: "Internal server error", status: false });
-    }
+    // Create date range for the entire day
+    const searchDate = new Date(dateTime);
+    const startOfDay = new Date(searchDate.setHours(0, 0, 0, 0));
+    const endOfDay = new Date(searchDate.setHours(23, 59, 59, 999));
+
+    const rides = await Ride.find({
+      startingPoint: { $regex: new RegExp(startLocation, "i") },
+      endingPoint: { $regex: new RegExp(destination, "i") },
+      date: { $gte: startOfDay, $lte: endOfDay },
+    }).populate("driver");
+
+    res.send(rides);
+  } catch (error) {
+    console.error("Error searching for rides:", error);
+    res.status(500).send({ msg: "Internal server error", status: false });
+  }
 };
 
 const createRide = async (req, res) => {
-    console.log(req.user);
-    try {
-        const data = {
-            startingPoint: req.body.startingPoint,
-            endingPoint: req.body.endingPoint,
-            // driver: req.user.id,
-            date: req.body.date,
-            time: req.body.time,
-            cost: req.body.cost,
+  console.log(req.user);
+  try {
+    const data = {
+      startingPoint: req.body.startingPoint,
+      endingPoint: req.body.endingPoint,
+      // driver: req.user.id,
+      date: req.body.date,
+      time: req.body.time,
+      cost: req.body.cost,
+    };
 
-        };
-
-        const newRide = await Ride.create(data);
-        res.status(200).send({ msg: "Ride created successfully", status: true, newRide });
-    } catch (error) {
-        console.log(error);
-        res.status(500).send({ msg: "Internal server error", status: false });
-    }
+    const newRide = await Ride.create(data);
+    res
+      .status(200)
+      .send({ msg: "Ride created successfully", status: true, newRide });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({ msg: "Internal server error", status: false });
+  }
 };
 
 const deleteRide = async (req, res) => {
-    // try {
-    //     const { id } = req.params;
-    //     await Ride.deleteOne({ _id: id });
-    //     res.status(200).send({ msg: "Ride deleted successfully", status: true });
-    // } catch (error) {
-    //     res.status(500).send({ msg: "Internal server error", status: false });
-    // }
+  // try {
+  //     const { id } = req.params;
+  //     await Ride.deleteOne({ _id: id });
+  //     res.status(200).send({ msg: "Ride deleted successfully", status: true });
+  // } catch (error) {
+  //     res.status(500).send({ msg: "Internal server error", status: false });
+  // }
 };
 
 const updateRide = async (req, res) => {
-    // try {
-    //     const { id } = req.params;
-    //     const data = req.body;
-    //     let updated = await Ride.updateOne({ _id: id }, data);
-    //     res.status(200).send({ msg: "Ride updated successfully", status: true, updated });
-    // } catch (error) {
-    //     res.status(500).send({ msg: "Internal server error", status: false });
-    // }
+  // try {
+  //     const { id } = req.params;
+  //     const data = req.body;
+  //     let updated = await Ride.updateOne({ _id: id }, data);
+  //     res.status(200).send({ msg: "Ride updated successfully", status: true, updated });
+  // } catch (error) {
+  //     res.status(500).send({ msg: "Internal server error", status: false });
+  // }
 };
 
 module.exports = { searchRides, createRide, deleteRide, updateRide };
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 // const Ride = require("../models/rideModel");
 
@@ -162,26 +122,3 @@ module.exports = { searchRides, createRide, deleteRide, updateRide };
 // };
 
 // module.exports = { searchRides, createRide, deleteRide, updateRide };
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
