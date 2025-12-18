abe8f17d0ef12fce4c8bafd2101f3aa5a464affe
dianakius <eingidiana@gmail.com>
2025-12-18 11:32:00 +0200

    fix: handle JWT verification errors properly

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -1,20 +1,21 @@
 const jwt = require("jsonwebtoken");
 require("dotenv").config();
+
 const verifyToken = (req, res, next) => {
-    const headers = req.headers.authorization;
-    //console.log("headers", headers);
-    if (!headers) {
-        return res.status(403).send({msg: "Token not found, Unauthorized"});
-    }
-    let ClientToken = headers.split(" ") [1];
+  const headers = req.headers.authorization;
+
+  if (!headers) {
+    return res.status(403).send({ msg: "Token not found, Unauthorized" });
+  }
+
+  try {
+    let ClientToken = headers.split(" ")[1];
     let verifiedToken = jwt.verify(ClientToken, process.env.PASSWORD);
-    //console.log(verifiedToken);
-    if (!verifiedToken) {
-        return res.status(401).send ({msg: "Invalid token, unauthorized"});
-    } else {
-        req.user = verifiedToken;
-        next();
-    }
+    req.user = verifiedToken;
+    next();
+  } catch (error) {
+    return res.status(401).send({ msg: "Invalid token, unauthorized" });
+  }
 };
 
-module.exports = verifyToken
+module.exports = verifyToken;
