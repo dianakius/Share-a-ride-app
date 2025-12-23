0a1a07ec9cbb60926d25504848918d98fbf17442
dianakius <eingidiana@gmail.com>
2025-12-23 16:29:18 +0200

    feature: create Testimonials component

---
+++ client/src/components/Testimonials.jsx
@@ -0,0 +1,11 @@
+import { useNavigate } from "react-router-dom";
+
+function Testimonials() {
+    return (
+        <div>
+            <h1>Testimonials</h1>
+        </div>
+    );
+}
+
+export default Testimonials;
