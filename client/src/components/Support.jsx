776fc2595b0e425244dcba574f48efca0e5244b2
dianakius <eingidiana@gmail.com>
2025-12-23 16:33:37 +0200

    feature: create Support component

---
+++ client/src/components/Support.jsx
@@ -0,0 +1,11 @@
+import { useNavigate } from "react-router-dom";
+
+function Support() {
+    return (
+        <div>
+            <h1>Support</h1>
+        </div>
+    );
+}
+
+export default Support;
