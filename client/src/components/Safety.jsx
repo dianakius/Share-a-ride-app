a46a834e60d31e3ccd30b9615e4b8ce80808ea20
dianakius <eingidiana@gmail.com>
2025-12-24 19:15:19 +0200

    refactor: create Safety component

---
+++ client/src/components/Safety.jsx
@@ -0,0 +1,11 @@
+import { useNavigate } from "react-router-dom";
+
+function Safety() {
+    return (
+        <div>
+            <h1>Safety</h1>
+        </div>
+    );
+}
+
+export default Safety;
