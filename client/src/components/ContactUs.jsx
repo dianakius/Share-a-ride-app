46c59b94b3800fe0d0b6407997c522e824a602ec
dianakius <eingidiana@gmail.com>
2025-12-30 14:18:09 +0200

    feature:create Contact-us component

---
+++ client/src/components/ContactUs.jsx
@@ -0,0 +1,11 @@
+import { useNavigate } from "react-router-dom";
+
+function ContactUs() {
+    return (
+        <div>
+            <h1>Contact Us</h1>
+        </div>
+    );
+}
+
+export default ContactUs;
