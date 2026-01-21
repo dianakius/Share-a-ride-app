ab41f9049f921778a9b70e7b978d4af0dae572c1
dianakius <eingidiana@gmail.com>
2026-01-21 14:25:11 +0200

    fix: add status

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -2,20 +2,20 @@ const jwt = require("jsonwebtoken");
 require("dotenv").config();
 
 const verifyToken = (req, res, next) => {
-  const headers = req.headers.authorization;
-
-  if (!headers) {
-    return res.status(403).send({ msg: "Token not found, Unauthorized" });
-  }
-
-  try {
-    let ClientToken = headers.split(" ")[1];
-    let verifiedToken = jwt.verify(ClientToken, process.env.PASSWORD);
-    req.user = verifiedToken;
-    next();
-  } catch (error) {
-    return res.status(401).send({ msg: "Invalid token, unauthorized" });
-  }
+    const headers = req.headers.authorization;
+    
+    if (!headers) {
+        return res.status(403).send({ msg: "Token not found, Unauthorized", status: false });
+    }
+    
+    try {
+        let clientToken = headers.split(" ")[1];
+        let verifiedToken = jwt.verify(clientToken, process.env.PASSWORD);
+        req.user = verifiedToken;
+        next();
+    } catch (error) {
+        return res.status(401).send({ msg: "Invalid token, unauthorized", status: false });
+    }
 };
 
 module.exports = verifyToken;
