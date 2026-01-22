7c1476697a6e9953cc293c8278f7db8258569025
dianakius <eingidiana@gmail.com>
2026-01-22 22:16:18 +0200

    feat: create new profile component

---
+++ client/src/components/Profile.jsx
@@ -0,0 +1,11 @@
+import { useNavigate } from "react-router-dom";
+
+function Profile() {
+    return (
+        <div>
+            <h1>Profile</h1>
+        </div>
+    );
+}
+
+export default Profile;
