afc5b3308a0e94d20ff3f11f4bc46958b3c03de1
dianakius <eingidiana@gmail.com>
2026-01-23 16:29:28 +0200

    feat: create a new AboutUs component when user is loggedIn

---
+++ client/src/components/AboutUs.jsx
@@ -0,0 +1,39 @@
+import React, { useState, useEffect } from 'react';
+import { useAuth } from './ContextAuth';
+import { useNavigate } from 'react-router-dom';
+
+function AboutUs() {
+  return (
+    <div>
+      <h1>
+        🌟 Introducing Your Ticket to Adventure: Share Ride! 🚗✨ Say goodbye to
+        the woes of expensive travel and hello to a whole new world of
+        convenience, savings, and friendship! With Share Ride, navigating from
+        city to city has never been more delightful or budget-friendly. 🎉 Hop
+        on the Joyride! 🎉 Gone are the days of fretting over pricey train
+        tickets or cramped bus schedules. With Share Ride, you're in the
+        driver's seat of your own journey. Whether you're off for a weekend
+        getaway, a business trip, or just exploring new horizons, we've got you
+        covered! 💰 Save Big, Travel Happy! 💰 Why break the bank when you can
+        share the ride and split the cost? With Share Ride, you're not just
+        traveling, you're embarking on an economical adventure. By teaming up
+        with fellow travelers headed in the same direction, you'll slash
+        expenses and maximize fun! 👫 Connect, Bond, Repeat! 👫 It's not just
+        about reaching your destination; it's about the unforgettable
+        connections you make along the way. Share Ride isn't just a
+        transportation solution; it's a vibrant community of like-minded
+        explorers. Forge new friendships, swap stories, and create memories
+        that'll last a lifetime. 🌍 The World Awaits...Let's Roll! 🌍 Ready to
+        hit the road with Share Ride? Whether you're a seasoned wanderer or a
+        first-time traveler, there's a seat with your name on it. Download the
+        app now, and let's embark on an epic journey together. Adventure is
+        calling, and with Share Ride, you're always just a click away from your
+        next great escapade! With Share Ride, the road ahead is filled with
+        endless possibilities and boundless smiles. Let's rev up those engines
+        and make every trip a remarkable one! 🚀🛣️
+      </h1>
+    </div>
+  );
+}
+
+export default AboutUs;
